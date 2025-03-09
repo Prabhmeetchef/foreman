cb729a8948ee828d1a4de733db22be3cd53fe590
Prabhmeet Singh <98380208+Prabhmeetchef@users.noreply.github.com>
2025-03-09 07:48:47 +0530

    Responsiveness enhanced

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -16,6 +16,7 @@ import {
   DropdownMenuShortcut,
 } from "@/components/ui/dropdown-menu";
 import ReactMarkdown from "react-markdown"; // Import react-markdown
+import MobileTopbar from "../../components/ui/mobile-topbar";
 export default function Dashboard() {
   const { data: session, status } = useSession();
   const router = useRouter();
@@ -77,6 +78,7 @@ export default function Dashboard() {
 
   return (
     <div className="flex h-screen bg-background justify-between">
+      < MobileTopbar />
       {/* Sidebar */}
       <div className="w-60 border-r bg-[#ede9e4] flex-col border-[#c6bdab] min-w-60 hidden sm:flex">
         <div className="p-4 border-[#c6bdab] border-b">
---
+++ src/components/ui/mobile-topbar.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import { useSession, signOut } from "next-auth/react";
+import Image from "next/image";
+import { Menu, X, Search, Settings, EllipsisVertical } from "lucide-react";
+import { useState } from "react";
+import Link from "next/link";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+interface MobileTopbarProps {
+  handleNewSession: () => void;
+}
+
+export default function MobileTopbar({ handleNewSession }: MobileTopbarProps) {
+  const { data: session } = useSession();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <div className="sm:hidden fixed top-0 left-0 w-full bg-[#ede9e4] p-4 flex justify-between items-center border-b border-[#c6bdab] z-50">
+      <Image src="/logo@1x.png" alt="logo" width={100} height={50} />
+
+      <button onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? <X className="text-amber-950" /> : <Menu className="text-amber-950" />}
+      </button>
+      {menuOpen && (
+        <div className="absolute top-full left-0 w-full bg-[#ede9e4] flex flex-col border-b border-[#c6bdab]">
+          {/* Session Controls */}
+
+          <div className="border-t border-[#c6bdab] py-2">
+            <h2 className="text-sm font-medium opacity-60 text-amber-950 px-2">Previously</h2>
+            <div className="w-full h-60">
+
+
+
+
+
+            </div>
+          </div>
+
+          {/* User Profile & Options */}
+          <div className="p-4 flex items-center gap-2 justify-between border-t border-[#c6bdab]">
+            <div className="flex gap-2">
+            {session?.user?.image ? (
+              <Image
+                src={session.user.image}
+                alt="avatar"
+                width={40}
+                height={40}
+                className="h-10 w-10 bg-amber-800 rounded-full"
+                onError={(e) => {
+                  e.currentTarget.onerror = null;
+                  e.currentTarget.src = "";
+                  e.currentTarget.style.display = "none";
+                  (e.currentTarget.nextElementSibling as HTMLElement).style.display = "flex";
+                }}
+              />
+            ) : (
+              <div className="h-10 w-10 bg-amber-50 text-amber-900 rounded-full flex items-center justify-center">
+                {session?.user?.name?.charAt(0)}
+              </div>
+            )}
+            <div className="flex flex-col">
+              <span className="text-sm font-medium text-amber-950">{session?.user?.name}</span>
+              <span className="text-xs w-[120px] truncate text-amber-900 opacity-60">{session?.user?.email}</span>
+            </div>
+            </div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild className="focus:outline-none pl-2">
+                <button>
+                  <EllipsisVertical className="size-[20px] text-amber-900" />
+                </button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-40 bg-white border border-[#c6bdab]">
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    Profile
+                    <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    Billing
+                    <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    Settings
+                    <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuItem className="hover:bg-amber-50">
+                  <a href="https://github.com/Prabhmeetchef/foreman" className="w-full h-full">GitHub</a>
+                </DropdownMenuItem>
+                <Link href="/contact">
+                  <DropdownMenuItem className="hover:bg-amber-50 hover:cursor-pointer">Support</DropdownMenuItem>
+                </Link>
+                <DropdownMenuItem disabled>API</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onClick={() => signOut({ callbackUrl: "/" })} className="hover:bg-red-400 hover:text-white">
+                  Log out
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
